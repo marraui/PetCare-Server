f367cb06d0f0fe8c65d59fce9b86b1dfa0016020
marraui <camilomarraui@gmail.com>
2019-09-10 22:43:59 -0500

    Log for debugging

--- src/controllers/getPets.ts
+++ src/controllers/getPets.ts
@@ -6,8 +6,11 @@ import { DatabaseManager } from '../models/dbmanager';
 import { Pet } from '../models/pet';
 
 export async function getPets(req: Request, res: Response) {
+    console.log('Get Pets -> Getting pets');
     const owner: User = req.body.owner;
     await DatabaseManager.insertUser(owner);
     const pets: Pet[] = await DatabaseManager.getPets(owner);
+    console.log('Get Pets -> Response:');
+    console.log(pets);
     res.status(HttpStatus.OK).json(pets);
 }
