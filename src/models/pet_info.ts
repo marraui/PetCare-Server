7a9eca269e58016d6fdbebaafd18ee12cd5155ca
marraui <camilomarraui@gmail.com>
2019-09-10 22:23:44 -0500

    Added temperature and heartBeat attributes

--- src/models/pet_info.ts
+++ src/models/pet_info.ts
@@ -5,10 +5,14 @@ export class PetInfo {
     pet: Pet;
     owner: User;
     date: Date;
+    temperature: number;
+    heartBeat: number;
 
     constructor (petInfo: any) {
         this.pet = petInfo && petInfo.pet || new Pet();
         this.owner = petInfo && petInfo.owner || new User();
         this.date = petInfo && petInfo.date || new Date();
+        this.temperature = petInfo && petInfo.temperature || null;
+        this.heartBeat = petInfo && petInfo.heartBeat || null;
     }
 }
