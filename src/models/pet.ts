4157d2fa9589c4f023e2ea425bffcc301b2b089f
marraui <camilomarraui@gmail.com>
2019-09-10 22:23:27 -0500

    Added typeOfPet attribute

--- src/models/pet.ts
+++ src/models/pet.ts
@@ -1,7 +1,12 @@
 export class Pet {
+    static CAT = 0;
+    static DOG = 1;
+
     name: string;
+    typeOfPet: string;
 
     constructor(pet?: any) {
         this.name = pet && pet.name || '';
+        this.typeOfPet = pet && pet.typeOfPet || 1;
     }
 }
