2fc3ea7d272ce1eea15347cd4885088ec77f96be
marraui <camilomarraui@gmail.com>
2019-09-16 22:16:35 -0500

    Logging messages for debugging

--- src/controllers/getPetInfo.ts
+++ src/controllers/getPetInfo.ts
@@ -11,10 +11,13 @@ export async function getPetInfo(req: Request, res: Response) {
     const owner: User = req.body.owner;
     const beginDate: number = req.body.beginDate ? req.body.beginDate : null;
     const endDate: number = req.body.endDate ? req.body.endDate : null;
-
+    console.log('Get Pet info -> body: ');
+    console.log(req.body);
 
     DatabaseManager.getPetInfo(pet, owner, beginDate, endDate).then((petInfo: PetInfo[]) => {
-        console.log('Get pet info -> Pet info retrieved succesfully');
+        console.log('Get Pet info -> Pet info retrieved succesfully');
+        console.log('Get Pet info -> Response: ');
+        console.log(petInfo);
         res.status(HttpStatus.OK).json(petInfo);
     }).catch(err => {
         console.log(`Get Pet info -> Couldn't retrieve pet info, error: ${err.message}`);
